58ea774f5cd0429bd34bbc3c07728be4a60e78a9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-12-03 16:36:31 -0800

    fix: get the search paths from python itself

--- nala-pyinstall.sh
+++ nala-pyinstall.sh
@@ -22,28 +22,46 @@ poetry install
 # make sure directories are clean
 rm -rf ./build/ ./dist/ ./**/__pycache__/
 
+# Get the venv paths list
+venv_paths=$(python3 -c '
+from site import getsitepackages
+from os import path, curdir
+
+paths = getsitepackages()
+args = map(lambda pth: f"--paths {path.relpath(pth, start = curdir)}", paths)
+print(" ".join(args))
+')
+
+# Get the system paths list
+system_paths=$(sudo python3 -c '
+from site import getsitepackages
+
+paths = getsitepackages()
+args = map(lambda pth: f"--paths {pth}", paths)
+print(" ".join(args))
+')
+
 pyinstaller --noconfirm \
---clean \
---console --nowindowed --noupx \
---paths "./.venv/lib/site-packages" \
---paths "/usr/lib/python3/dist-packages" \
---paths "/lib/python3/dist-packages/" \
---exclude-module IPython \
---exclude-module IPython.display \
---exclude-module IPython.core \
---exclude-module IPython.core.formatters \
---exclude-module ipywidgets \
---exclude-module java \
---exclude-module java.lang \
---exclude-module winreg \
---exclude-module _winreg \
---exclude-module _winapi \
---exclude-module win32api \
---exclude-module win32com \
---exclude-module win32com.shell \
---exclude-module msvcrt \
---name nala \
-./nala/__main__.py
+    --clean \
+    --console --nowindowed --noupx \
+    $venv_paths \
+    $system_paths \
+    --exclude-module IPython \
+    --exclude-module IPython.display \
+    --exclude-module IPython.core \
+    --exclude-module IPython.core.formatters \
+    --exclude-module ipywidgets \
+    --exclude-module java \
+    --exclude-module java.lang \
+    --exclude-module winreg \
+    --exclude-module _winreg \
+    --exclude-module _winapi \
+    --exclude-module win32api \
+    --exclude-module win32com \
+    --exclude-module win32com.shell \
+    --exclude-module msvcrt \
+    --name nala \
+    ./nala/__main__.py
 
 # Remove the excluded modules from the warnings list
 sed -i '/excluded module /d' ./build/nala/warn-nala.txt
@@ -52,7 +70,7 @@ sed -i '/excluded module /d' ./build/nala/warn-nala.txt
 ./dist/nala/nala --help
 
 # Archive the build and deactivate the virtual env
-cd ./dist && tar cv nala/ | xz -9 > ./nala.tar.xz
+cd ./dist && tar cv nala/ | xz -9 >./nala.tar.xz
 deactivate
 
 # TODO add docs to the pyinstaller
